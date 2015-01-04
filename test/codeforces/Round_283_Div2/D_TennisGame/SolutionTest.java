46d3605b01c3066b2894af5a857a03957d4d686b
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2015-01-04 14:50:28 +0300

    Added all tests for D_TennisGame.

--- test/codeforces/Round_283_Div2/D_TennisGame/SolutionTest.java
+++ test/codeforces/Round_283_Div2/D_TennisGame/SolutionTest.java
@@ -9,16 +9,54 @@ import utils.Utils;
 public class SolutionTest {
 
     @Test
-    public void firstTest() {
+    public void test1() {
         OutputGrabber grabber = new OutputGrabber();
 
         Solution.parseSolveAndPrint(Utils.toStream(
-                                    "5\n" +
-                                    "1 2 1 2 1\n"), grabber.writer());
+                                    "5\n1 2 1 2 1\n"), grabber.writer());
 
         Assert.assertEquals(grabber.content(),
                 "2\n" +
-                "1 3\n" +
-                "3 1\n");
+                        "1 3\n" +
+                        "3 1\n");
+    }
+
+    @Test
+    public void test2() {
+        OutputGrabber grabber = new OutputGrabber();
+
+        Solution.parseSolveAndPrint(Utils.toStream(
+                "4\n1 1 1 1\n"), grabber.writer());
+
+        Assert.assertEquals(grabber.content(),
+                "3\n" +
+                "1 4\n" +
+                "2 2\n" +
+                "4 1\n");
+    }
+
+    @Test
+    public void test3() {
+        OutputGrabber grabber = new OutputGrabber();
+
+        Solution.parseSolveAndPrint(Utils.toStream(
+                "4\n1 2 1 2\n"), grabber.writer());
+
+        Assert.assertEquals(grabber.content(),
+                "0\n");
+    }
+
+    @Test
+    public void test4() {
+        OutputGrabber grabber = new OutputGrabber();
+
+        Solution.parseSolveAndPrint(Utils.toStream(
+                "8\n2 1 2 1 1 1 1 1\n"), grabber.writer());
+
+        Assert.assertEquals(grabber.content(),
+                "3\n" +
+                "1 6\n" +
+                "2 3\n" +
+                "6 1\n");
     }
 }
